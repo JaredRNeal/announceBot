cf300c42d05f0fb3a8d2c70704e748c0bbdc9385
mrkirby153 <mr.austinwhyte@gmail.com>
2018-10-23 14:22:26 -0700

    Add token parsers for summary messages

--- commands/stats.py
+++ commands/stats.py
@@ -1,4 +1,6 @@
 import re
+from datetime import datetime, timedelta
+from functools import reduce
 
 from disco.bot import Plugin
 from disco.bot.command import CommandEvent
@@ -14,6 +16,7 @@ class StatsPlugin(Plugin):
     def __init__(self, bot, config):
         super().__init__(bot, config)
         self.channel_regex = re.compile('<#([0-9]{17,18})>\\s.*Reported:', re.MULTILINE)
+        self.argument_regex = re.compile('{{([A-Za-z_]+):?([A-Za-z0-9,_/]+)?}}', re.MULTILINE)
 
     @Plugin.command('reportingChannel', '<chan_id:snowflake> <message_id:snowflake>')
     def reporting_channel(self, event: CommandEvent, chan_id, message_id):
@@ -34,7 +37,7 @@ class StatsPlugin(Plugin):
         if reports is not None:
             body = ""
             for chan, reports in reports.items():
-                to_add = f"<#{chan}> - **{len(reports)} report" + ("s" if len(reports) > 1 else "")+"**\n"
+                to_add = f"<#{chan}> - **{len(reports)} report" + ("s" if len(reports) > 1 else "") + "**\n"
                 if len(to_add + body) > 2000:
                     event.msg.reply(body)
                     body = ""
@@ -42,6 +45,10 @@ class StatsPlugin(Plugin):
             event.msg.reply(body)
         else:
             event.msg.reply("no reports in the queue?")
+
+    @Plugin.command('argTest', '<msg:str...>')
+    def arg_test(self, event: CommandEvent, msg: str):
+        event.msg.reply(self.parse_message(msg, self.get_all_bug_reports()))
 
     def get_reporting_channel(self, msg: Message):
         match = self.channel_regex.findall(msg.content)
@@ -64,7 +71,53 @@ class StatsPlugin(Plugin):
             if bug_report_channel is None:
                 continue  # Failed the Regex.
             if bug_report_channel not in reports.keys():
-                reports[bug_report_channel] = [message.id]
+                reports[bug_report_channel] = [message]
             else:
-                reports[bug_report_channel].append(message.id)
+                reports[bug_report_channel].append(message)
         return reports
+
+    def parse_message(self, msg, reports):
+        parsed = msg
+        for match in self.argument_regex.finditer(msg):
+            arg = match.group(0)
+            name = match.group(1).lower()
+            args = match.group(2).lower().split(",")
+            self.bot.log.info(f"Parsing {arg} -- Name: {name}, args: {args}")
+            resp = self.call_arguments(name, args, reports)
+            parsed = parsed.replace(arg, resp)
+        return parsed
+
+    def call_arguments(self, argument_type, params, reports):
+        self.bot.log.info(f"Executing argument_{argument_type}")
+        return getattr(self, f"argument_{argument_type}")(**{'params': params, 'reports': reports})
+
+    def argument_oldest_report(self, params, reports: dict):
+        r = reduce(list.__add__, reports.values()) if params[0] == "all" \
+            else reports[params[0]] if params[0] in reports.keys() else []
+        if len(r) == 0:
+            return ""
+        report = sorted(r, key=lambda x: x.id)[0]
+        return f"https://discordapp.com/channels/{report.guild.id}/{report.channel.id}/{report.id}"
+
+    def argument_total_reports(self, params, reports: dict):
+        r = reduce(list.__add__, reports.values()) if params[0] == "all" \
+            else reports[params[0]] if params[0] in reports.keys() else []
+        return str(len(r))
+
+    def argument_stale_reports(self, params, reports: dict):
+        r = reduce(list.__add__, reports.values()) if params[0] == "all" \
+            else reports[params[0]] if params[0] in reports.keys() else []
+        hours = params[1]
+        target = datetime.today() - timedelta(hours=int(hours))
+        target_snowflake = snowflake.from_datetime(target)
+
+        stale_reports = []
+        for report in r:
+            if report.id < target_snowflake:
+                if report.edited_timestamp is not None:
+                    if report.edited_timestamp < target:
+                        stale_reports.append(report)
+                else:
+                    stale_reports.append(report)
+
+        return str(len(stale_reports))
