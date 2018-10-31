8316261b139016137777f1bc963416e64192c28d
JaredRNeal <jared@discordapp.com>
2018-10-31 10:41:41 -0700

    The initial commit didn't work, made changes so it does.

--- commands/github.py
+++ commands/github.py
@@ -1,9 +1,10 @@
 from commands.config import GitHubConfig
 from disco.bot import Plugin
+from util.GlobalHandlers import command_wrapper, log_to_bot_log
 
 
 @Plugin.with_config(GitHubConfig)
-class GuidePlugin(Plugin):
+class GithubPlugin(Plugin):
 
     def load(self, ctx):
         super().load(ctx)
@@ -14,4 +15,5 @@ class GuidePlugin(Plugin):
     @Plugin.command("github", aliases=["source"])
     @command_wrapper(perm_lvl=0, allowed_in_dm=True, allowed_on_server=True)
     def github(self, event):
-        await event.msg.reply(f"My source code can be found at <{self.config.source_code_location}>")
+        event.msg.reply(f"My source code can be found at <{self.config.source_code_location}>")
+        log_to_bot_log(self.bot, ":bug: " + str(event.msg.author) + " used the Github command.")
